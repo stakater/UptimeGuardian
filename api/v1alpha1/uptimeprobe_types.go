8910282f85ed84ee823bd6a02d03c8acd11457cc
Juansasa <quavun@gmail.com>
2025-01-31 09:04:38 +0100

    Added Open Cluster Management API

--- api/v1alpha1/uptimeprobe_types.go
+++ api/v1alpha1/uptimeprobe_types.go
@@ -20,16 +20,8 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // UptimeProbeSpec defines the desired state of UptimeProbe
 type UptimeProbeSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of UptimeProbe. Edit uptimeprobe_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
 }
 
 // UptimeProbeStatus defines the observed state of UptimeProbe
