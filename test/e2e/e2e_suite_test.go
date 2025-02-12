db8f5741f8e980a2f189bc949758f64f6f2fe9b2
Juansasa <quavun@gmail.com>
2025-02-12 09:50:31 +0100

    Fixed lint

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -27,6 +27,6 @@ import (
 // Run e2e tests using the Ginkgo runner.
 func TestE2E(t *testing.T) {
 	RegisterFailHandler(Fail)
-	fmt.Fprintf(GinkgoWriter, "Starting uptimeguardian suite\n")
+	_, _ = fmt.Fprintf(GinkgoWriter, "Starting uptimeguardian suite\n")
 	RunSpecs(t, "e2e suite")
 }
