89c93ed2d82cbf13e81d1b2d481443c0ccf73d21
Fateh Ullah <ufateh@outlook.com>
2025-03-12 11:11:12 +0100

    fixed the failing test, it was failing on stopping the channel, just removed it as we do not need it

--- internal/controller/spoke_cluster_manager_controller_test.go
+++ internal/controller/spoke_cluster_manager_controller_test.go
@@ -240,13 +240,12 @@ var _ = Describe("SpokeClusterManagerController", func() {
 			reconciler.RemoteClients[clientKey] = mockSpokeManager
 
 			// Try to setup host client again
-			stopChan, err := reconciler.setupRemoteClientForHostCluster()
+			_, err := reconciler.setupRemoteClientForHostCluster()
 			Expect(err).NotTo(HaveOccurred())
 
 			// Verify the original client was not replaced
-			hostClient := reconciler.RemoteClients["host"]
+			hostClient := reconciler.RemoteClients[clientKey]
 			Expect(hostClient).To(Equal(mockSpokeManager))
-			close(stopChan)
 		})
 	})
 })
